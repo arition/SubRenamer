0886303aa6b1256bcd3d7158180516591e5b8a16
arition <aritionkb@gmail.com>
2017-01-01 22:44:20 -0800

    add icons and cleanup

--- SubRenamer/Properties/AssemblyInfo.cs
+++ SubRenamer/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SubRenamer")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © arition 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
