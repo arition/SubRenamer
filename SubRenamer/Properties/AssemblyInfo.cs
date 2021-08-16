22565e8361f9170d6ee5f41dd4391a668c185309
arition <aritionkb@gmail.com>
2021-08-16 00:33:14 -0700

    bump version

--- SubRenamer/Properties/AssemblyInfo.cs
+++ SubRenamer/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SubRenamer")]
-[assembly: AssemblyCopyright("Copyright © arition 2017")]
+[assembly: AssemblyCopyright("Copyright © arition 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
