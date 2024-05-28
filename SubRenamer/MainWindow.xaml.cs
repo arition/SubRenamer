a29595c9fa53eb6466a03fc0fc06bf2cff894270
arition <aritionkb@gmail.com>
2024-05-27 21:56:22 -0700

    add support for using command line args as input; add support for drag and drop folders

--- SubRenamer/MainWindow.xaml.cs
+++ SubRenamer/MainWindow.xaml.cs
@@ -13,192 +13,194 @@ using Microsoft.Win32;
 using NLog;
 using SubRenamer.Annotations;
 
-namespace SubRenamer
+namespace SubRenamer;
+
+/// <summary>
+///     MainWindow.xaml 的交互逻辑
+/// </summary>
+public partial class MainWindow : INotifyPropertyChanged
 {
-    /// <summary>
-    ///     MainWindow.xaml 的交互逻辑
-    /// </summary>
-    public partial class MainWindow : INotifyPropertyChanged
+    private bool _copySub;
+    private bool _eatSushi;
+    private string _subtitleFileExtension;
+
+    public MainWindow()
     {
-        private bool _copySub;
-        private bool _eatSushi;
-        private string _subtitleFileExtension;
+        DataContext = this;
+        InitializeComponent();
+        Column = GridView.Columns[0];
+        GridView.Columns.RemoveAt(0);
 
-        public MainWindow()
+        // watch theme
+        if (OperatingSystem.IsWindows())
         {
-            DataContext = this;
-            InitializeComponent();
-            Column = GridView.Columns[0];
-            GridView.Columns.RemoveAt(0);
-
-            if (OperatingSystem.IsWindows())
-            {
-                Theme.OnWindowsThemeChanged += ((sender, theme) =>
-                    Dispatcher.Invoke(() =>
-                        ThemeManager.Current.ChangeTheme(this,
-                            theme == WindowsTheme.Theme.Light ? "Light.Steel" : "Dark.Steel")));
-                Theme.WatchTheme();
-            }
+            Theme.OnWindowsThemeChanged += (sender, theme) =>
+                Dispatcher.Invoke(() =>
+                    ThemeManager.Current.ChangeTheme(this,
+                        theme == WindowsTheme.Theme.Light ? "Light.Steel" : "Dark.Steel"));
+            Theme.WatchTheme();
         }
 
-        private GridViewColumn Column { get; }
-        public ModelList ModelList { get; } = new();
-        private Logger Logger { get; } = LogManager.GetCurrentClassLogger();
-        private Logger SushiLogger { get; } = LogManager.GetLogger("Sushi");
-        private WindowsTheme Theme { get; } = new();
+        // add files from command line
+        var args = Environment.GetCommandLineArgs();
+        ModelList.AddDropFiles(args, EatSushi);
+    }
 
-        public string SubtitleFileExtension
+    private GridViewColumn Column { get; }
+    public ModelList ModelList { get; } = new();
+    private Logger Logger { get; } = LogManager.GetCurrentClassLogger();
+    private Logger SushiLogger { get; } = LogManager.GetLogger("Sushi");
+    private WindowsTheme Theme { get; } = new();
+
+    public string SubtitleFileExtension
+    {
+        get => _subtitleFileExtension;
+        set
         {
-            get => _subtitleFileExtension;
-            set
-            {
-                ModelList.SubtitleFileExtension = value;
-                _subtitleFileExtension = value;
-                OnPropertyChanged();
-            }
+            ModelList.SubtitleFileExtension = value;
+            _subtitleFileExtension = value;
+            OnPropertyChanged();
         }
+    }
 
-        public bool CopySub
+    public bool CopySub
+    {
+        get => _copySub;
+        set
         {
-            get => _copySub;
-            set
-            {
-                _copySub = value;
-                OnPropertyChanged();
-            }
+            _copySub = value;
+            OnPropertyChanged();
         }
+    }
 
-        public bool EatSushi
+    public bool EatSushi
+    {
+        get => _eatSushi;
+        set
         {
-            get => _eatSushi;
-            set
-            {
-                _eatSushi = value;
-                if (_eatSushi)
-                    GridView.Columns.Insert(0, Column);
-                else
-                    GridView.Columns.RemoveAt(0);
-                OnPropertyChanged();
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void BtnSelectOriginalMovie_OnClick(object sender, RoutedEventArgs e)
-        {
-            var selectFile = new OpenFileDialog
-            {
-                Multiselect = true,
-                Filter = "视频文件(*.mp4;*.m4v;*.mkv;*.m2ts;*.webm)|*.mp4;*.m4v;*.mkv;*.m2ts;*.webm|所有文件 (*.*)|*.*"
-            };
-            if (selectFile.ShowDialog() == true) ModelList.AddOriginalMovie(selectFile.FileNames);
-        }
-
-        private void BtnSelectMovie_OnClick(object sender, RoutedEventArgs e)
-        {
-            var selectFile = new OpenFileDialog
-            {
-                Multiselect = true,
-                Filter = "视频文件(*.mp4;*.m4v;*.mkv;*.m2ts;*.webm)|*.mp4;*.m4v;*.mkv;*.m2ts;*.webm|所有文件 (*.*)|*.*"
-            };
-            if (selectFile.ShowDialog() == true) ModelList.AddMovie(selectFile.FileNames);
-        }
-
-        private void BtnSelectSub_OnClick(object sender, RoutedEventArgs e)
-        {
-            var selectFile = new OpenFileDialog
-            {
-                Multiselect = true,
-                Filter = "字幕文件(*.ass;*.ssa;*.srt;*.sub;*.idx)|*.ass;*.ssa;*.srt;*.sub;*.idx|所有文件 (*.*)|*.*"
-            };
-            if (selectFile.ShowDialog() == true) ModelList.AddSub(selectFile.FileNames);
-        }
-
-        private async void BtnRename_OnClick(object sender, RoutedEventArgs e)
-        {
-            ProgressDialogController controller = null;
-            if (EatSushi)
-            {
-                controller = await this.ShowProgressAsync("正在处理", "正在处理第1个字幕");
-                controller.Minimum = 0;
-                controller.Maximum = 1;
-            }
-
-            var sb = new StringBuilder();
-            for (var i = 0; i < ModelList.Models.Count; i++)
-            {
-                if (EatSushi) controller?.SetMessage($"正在处理第{i + 1}个字幕");
-                Logger.Info($"正在处理第{i + 1}个字幕");
-                var model = ModelList.Models[i];
-                try
-                {
-                    model.GenerateRenameSubFiles(CopySub);
-                    for (var j = 0; j < model.SubFiles.Count; j++)
-                    {
-                        if (!EatSushi)
-                        {
-                            if (CopySub) model.SubFiles[j].CopyTo(model.RenamedSubFiles[j].FullName);
-                            else model.SubFiles[j].MoveTo(model.RenamedSubFiles[j].FullName);
-                        }
-                        else
-                        {
-                            var process = new Process
-                            {
-                                StartInfo = new ProcessStartInfo
-                                {
-                                    FileName = Path.Combine("Sushi", "sushi.exe"),
-                                    Arguments = $"--src \"{model.OriginalMovieFile.FullName}\" " +
-                                                $"--dst \"{model.MovieFile.FullName}\" " +
-                                                $"--script \"{model.SubFiles[j].FullName}\" " +
-                                                $"-o \"{model.RenamedSubFiles[j].FullName}\"",
-                                    UseShellExecute = false,
-                                    CreateNoWindow = true,
-                                    RedirectStandardOutput = true,
-                                    RedirectStandardError = true
-                                }
-                            };
-                            process.OutputDataReceived += (senderx, ex) => SushiLogger.Info(ex.Data);
-                            process.ErrorDataReceived += (senderx, ex) => SushiLogger.Info(ex.Data);
-                            process.Start();
-                            await Task.Run(() => process.WaitForExit());
-                            if (!CopySub) model.SubFiles[j].Delete();
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    sb.AppendLine($"第{i + 1}个字幕出现错误：{ex.Message}");
-                    Logger.Error(ex, $"第{i + 1}个字幕出现错误");
-                }
-
-                if (EatSushi) controller?.SetProgress((i + 1d) / ModelList.Models.Count);
-            }
-
-            if (EatSushi && controller != null) await controller.CloseAsync();
-            Logger.Info("重命名完成");
-            var message = sb.ToString();
-            if (string.IsNullOrWhiteSpace(message))
-                await this.ShowMessageAsync("成功", "重命名成功");
+            _eatSushi = value;
+            if (_eatSushi)
+                GridView.Columns.Insert(0, Column);
             else
-                await this.ShowMessageAsync("错误", message);
-            ModelList.Models.Clear();
+                GridView.Columns.RemoveAt(0);
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void BtnSelectOriginalMovie_OnClick(object sender, RoutedEventArgs e)
+    {
+        var selectFile = new OpenFileDialog
+        {
+            Multiselect = true,
+            Filter = "视频文件(*.mp4;*.m4v;*.mkv;*.m2ts;*.webm)|*.mp4;*.m4v;*.mkv;*.m2ts;*.webm|所有文件 (*.*)|*.*"
+        };
+        if (selectFile.ShowDialog() == true) ModelList.AddOriginalMovie(selectFile.FileNames);
+    }
+
+    private void BtnSelectMovie_OnClick(object sender, RoutedEventArgs e)
+    {
+        var selectFile = new OpenFileDialog
+        {
+            Multiselect = true,
+            Filter = "视频文件(*.mp4;*.m4v;*.mkv;*.m2ts;*.webm)|*.mp4;*.m4v;*.mkv;*.m2ts;*.webm|所有文件 (*.*)|*.*"
+        };
+        if (selectFile.ShowDialog() == true) ModelList.AddMovie(selectFile.FileNames);
+    }
+
+    private void BtnSelectSub_OnClick(object sender, RoutedEventArgs e)
+    {
+        var selectFile = new OpenFileDialog
+        {
+            Multiselect = true,
+            Filter = "字幕文件(*.ass;*.ssa;*.srt;*.sub;*.idx)|*.ass;*.ssa;*.srt;*.sub;*.idx|所有文件 (*.*)|*.*"
+        };
+        if (selectFile.ShowDialog() == true) ModelList.AddSub(selectFile.FileNames);
+    }
+
+    private async void BtnRename_OnClick(object sender, RoutedEventArgs e)
+    {
+        ProgressDialogController controller = null;
+        if (EatSushi)
+        {
+            controller = await this.ShowProgressAsync("正在处理", "正在处理第1个字幕");
+            controller.Minimum = 0;
+            controller.Maximum = 1;
         }
 
-        private void BtnClearList_OnClick(object sender, RoutedEventArgs e)
+        var sb = new StringBuilder();
+        for (var i = 0; i < ModelList.Models.Count; i++)
         {
-            ModelList.Models.Clear();
+            if (EatSushi) controller?.SetMessage($"正在处理第{i + 1}个字幕");
+            Logger.Info($"正在处理第{i + 1}个字幕");
+            var model = ModelList.Models[i];
+            try
+            {
+                model.GenerateRenameSubFiles(CopySub);
+                for (var j = 0; j < model.SubFiles.Count; j++)
+                    if (!EatSushi)
+                    {
+                        if (CopySub) model.SubFiles[j].CopyTo(model.RenamedSubFiles[j].FullName);
+                        else model.SubFiles[j].MoveTo(model.RenamedSubFiles[j].FullName);
+                    }
+                    else
+                    {
+                        var process = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = Path.Combine("Sushi", "sushi.exe"),
+                                Arguments = $"--src \"{model.OriginalMovieFile.FullName}\" " +
+                                            $"--dst \"{model.MovieFile.FullName}\" " +
+                                            $"--script \"{model.SubFiles[j].FullName}\" " +
+                                            $"-o \"{model.RenamedSubFiles[j].FullName}\"",
+                                UseShellExecute = false,
+                                CreateNoWindow = true,
+                                RedirectStandardOutput = true,
+                                RedirectStandardError = true
+                            }
+                        };
+                        process.OutputDataReceived += (senderx, ex) => SushiLogger.Info(ex.Data);
+                        process.ErrorDataReceived += (senderx, ex) => SushiLogger.Info(ex.Data);
+                        process.Start();
+                        await Task.Run(() => process.WaitForExit());
+                        if (!CopySub) model.SubFiles[j].Delete();
+                    }
+            }
+            catch (Exception ex)
+            {
+                sb.AppendLine($"第{i + 1}个字幕出现错误：{ex.Message}");
+                Logger.Error(ex, $"第{i + 1}个字幕出现错误");
+            }
+
+            if (EatSushi) controller?.SetProgress((i + 1d) / ModelList.Models.Count);
         }
 
-        private void ListInfo_OnDrop(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetData(DataFormats.FileDrop) is string[] files)
-                ModelList.AddDropFiles(files, EatSushi);
-        }
+        if (EatSushi && controller != null) await controller.CloseAsync();
+        Logger.Info("重命名完成");
+        var message = sb.ToString();
+        if (string.IsNullOrWhiteSpace(message))
+            await this.ShowMessageAsync("成功", "重命名成功");
+        else
+            await this.ShowMessageAsync("错误", message);
+        ModelList.Models.Clear();
+    }
 
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+    private void BtnClearList_OnClick(object sender, RoutedEventArgs e)
+    {
+        ModelList.Models.Clear();
+    }
+
+    private void ListInfo_OnDrop(object sender, DragEventArgs e)
+    {
+        if (e.Data.GetData(DataFormats.FileDrop) is string[] files)
+            ModelList.AddDropFiles(files, EatSushi);
+    }
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- SubRenamer/ModelList.cs
+++ SubRenamer/ModelList.cs
@@ -4,162 +4,159 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 
-namespace SubRenamer
+namespace SubRenamer;
+
+public class ModelList
 {
-    public class ModelList
+    private string _subtitleFileExtension;
+
+    public ObservableCollection<Model> Models { get; } = new();
+
+    public string SubtitleFileExtension
     {
-        private string _subtitleFileExtension;
-
-        public ObservableCollection<Model> Models { get; } = new();
-
-        public string SubtitleFileExtension
+        get => _subtitleFileExtension;
+        set
         {
-            get => _subtitleFileExtension;
-            set
-            {
-                foreach (var model in Models) model.SubtitleFileExtension = value;
+            foreach (var model in Models) model.SubtitleFileExtension = value;
 
-                _subtitleFileExtension = value;
-            }
+            _subtitleFileExtension = value;
         }
+    }
 
-        public void AddOriginalMovie(IEnumerable<string> files)
+    public void AddOriginalMovie(IEnumerable<string> files)
+    {
+        var fileList = files.OrderBy(t => t);
+        var i = 0;
+        foreach (var selectFileFileName in fileList)
         {
-            var fileList = files.OrderBy(t => t);
-            var i = 0;
-            foreach (var selectFileFileName in fileList)
-            {
-                if (i == Models.Count)
+            if (i == Models.Count)
+                Models.Add(new Model
                 {
-                    Models.Add(new Model
-                    {
-                        OriginalMovieFile = new FileInfo(selectFileFileName),
-                        SubtitleFileExtension = SubtitleFileExtension
-                    });
-                }
-                else
-                    Models[i].OriginalMovieFile = new FileInfo(selectFileFileName);
-
-                i++;
-            }
-
-            while (i < Models.Count) Models.RemoveAt(i);
-        }
-
-        public void AddMovie(IEnumerable<string> files)
-        {
-            var fileList = files.OrderBy(t => t);
-            var i = 0;
-            foreach (var selectFileFileName in fileList)
-            {
-                if (i == Models.Count)
-                {
-                    Models.Add(new Model
-                    {
-                        MovieFile = new FileInfo(selectFileFileName),
-                        SubtitleFileExtension = SubtitleFileExtension
-                    });
-                }
-                else
-                    Models[i].MovieFile = new FileInfo(selectFileFileName);
-
-                i++;
-            }
-
-            while (i < Models.Count) Models.RemoveAt(i);
-        }
-
-        public void AddSub(IEnumerable<string> files)
-        {
-            var fileList = files.Select(t => new
-            {
-                file = new FileInfo(t),
-                name = new FileInfo(t).Name
-            }).Select(t => new
-            {
-                t.file,
-                t.name,
-                nameOnly = t.name.Substring(0,
-                    t.name.Substring(t.name.Length - 15 >= 0 ? t.name.Length - 15 : 0)
-                        .IndexOf(".", StringComparison.Ordinal) + (t.name.Length - 15 >= 0 ? t.name.Length - 15 : 0))
-            }).OrderBy(t => t.nameOnly).ToList();
-
-            var i = -1;
-            var lastSubNameOnly = "";
-
-            foreach (var selectFileFileName in fileList)
-            {
-                if (lastSubNameOnly != selectFileFileName.nameOnly)
-                {
-                    i++;
-                    if (i < Models.Count) Models[i].SubFiles.Clear();
-                }
-
-                if (i == Models.Count)
-                {
-                    var model = new Model { SubtitleFileExtension = SubtitleFileExtension };
-                    model.SubFiles.Add(selectFileFileName.file);
-                    Models.Add(model);
-                }
-                else
-                    Models[i].SubFiles.Add(selectFileFileName.file);
-
-                lastSubNameOnly = selectFileFileName.nameOnly;
-            }
+                    OriginalMovieFile = new FileInfo(selectFileFileName),
+                    SubtitleFileExtension = SubtitleFileExtension
+                });
+            else
+                Models[i].OriginalMovieFile = new FileInfo(selectFileFileName);
 
             i++;
-            while (i < Models.Count) Models.RemoveAt(i);
         }
 
-        public void AddDropFiles(IEnumerable<string> files, bool eatSushi)
+        while (i < Models.Count) Models.RemoveAt(i);
+    }
+
+    public void AddMovie(IEnumerable<string> files)
+    {
+        var fileList = files.OrderBy(t => t);
+        var i = 0;
+        foreach (var selectFileFileName in fileList)
         {
-            var originalMovieList = new List<string>();
-            var movieList = new List<string>();
-            var subList = new List<string>();
-            foreach (var file in files)
-            {
-                var extension = new FileInfo(file).Extension.ToLower();
-                switch (extension)
+            if (i == Models.Count)
+                Models.Add(new Model
                 {
-                    case ".mp4":
-                    case ".m4v":
-                    case ".mkv":
-                    case ".m2ts":
-                    case ".webm":
-                        if (!eatSushi || movieList.Count == 0)
-                            movieList.Add(file);
-                        else if (Utils.TestSimilarity(new FileInfo(movieList[0]).Name, new FileInfo(file).Name) > 30)
-                            movieList.Add(file);
-                        else
-                            originalMovieList.Add(file);
-                        break;
-                    case ".ass":
-                    case ".ssa":
-                    case ".srt":
-                    case ".sub":
-                    case ".idx":
-                        subList.Add(file);
-                        break;
-                }
-            }
+                    MovieFile = new FileInfo(selectFileFileName),
+                    SubtitleFileExtension = SubtitleFileExtension
+                });
+            else
+                Models[i].MovieFile = new FileInfo(selectFileFileName);
 
-            if (subList.Count > 0) AddSub(subList);
-            if (eatSushi)
-            {
-                if (Models.Count > 0 && !string.IsNullOrWhiteSpace(Models[0].SubFileName))
-                {
-                    if (movieList.Count > 0)
-                    {
-                        if (Utils.TestSimilarity(Models[0].SubFiles[0].Name, new FileInfo(movieList[0]).Name) > 30)
-                        {
-                            (movieList, originalMovieList) = (originalMovieList, movieList);
-                        }
-                    }
-                }
-            }
-
-            if (originalMovieList.Count > 0) AddOriginalMovie(originalMovieList);
-            if (movieList.Count > 0) AddMovie(movieList);
+            i++;
         }
+
+        while (i < Models.Count) Models.RemoveAt(i);
+    }
+
+    public void AddSub(IEnumerable<string> files)
+    {
+        var fileList = files.Select(t => new
+        {
+            file = new FileInfo(t),
+            name = new FileInfo(t).Name
+        }).Select(t => new
+        {
+            t.file,
+            t.name,
+            nameOnly = t.name.Substring(0,
+                t.name.Substring(t.name.Length - 15 >= 0 ? t.name.Length - 15 : 0)
+                    .IndexOf(".", StringComparison.Ordinal) + (t.name.Length - 15 >= 0 ? t.name.Length - 15 : 0))
+        }).OrderBy(t => t.nameOnly).ToList();
+
+        var i = -1;
+        var lastSubNameOnly = "";
+
+        foreach (var selectFileFileName in fileList)
+        {
+            if (lastSubNameOnly != selectFileFileName.nameOnly)
+            {
+                i++;
+                if (i < Models.Count) Models[i].SubFiles.Clear();
+            }
+
+            if (i == Models.Count)
+            {
+                var model = new Model { SubtitleFileExtension = SubtitleFileExtension };
+                model.SubFiles.Add(selectFileFileName.file);
+                Models.Add(model);
+            }
+            else
+            {
+                Models[i].SubFiles.Add(selectFileFileName.file);
+            }
+
+            lastSubNameOnly = selectFileFileName.nameOnly;
+        }
+
+        i++;
+        while (i < Models.Count) Models.RemoveAt(i);
+    }
+
+    public void AddDropFiles(IEnumerable<string> paths, bool eatSushi)
+    {
+        var originalMovieList = new List<string>();
+        var movieList = new List<string>();
+        var subList = new List<string>();
+        var files = paths.SelectMany(path =>
+        {
+            if (Directory.Exists(path))
+                return Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly);
+            if (File.Exists(path))
+                return new[] { path };
+            return Array.Empty<string>();
+        });
+        foreach (var file in files)
+        {
+            var extension = new FileInfo(file).Extension.ToLower();
+            switch (extension)
+            {
+                case ".mp4":
+                case ".m4v":
+                case ".mkv":
+                case ".m2ts":
+                case ".webm":
+                    if (!eatSushi || movieList.Count == 0)
+                        movieList.Add(file);
+                    else if (Utils.TestSimilarity(new FileInfo(movieList[0]).Name, new FileInfo(file).Name) > 30)
+                        movieList.Add(file);
+                    else
+                        originalMovieList.Add(file);
+                    break;
+                case ".ass":
+                case ".ssa":
+                case ".srt":
+                case ".sub":
+                case ".idx":
+                    subList.Add(file);
+                    break;
+            }
+        }
+
+        if (subList.Count > 0) AddSub(subList);
+        if (eatSushi)
+            if (Models.Count > 0 && !string.IsNullOrWhiteSpace(Models[0].SubFileName))
+                if (movieList.Count > 0)
+                    if (Utils.TestSimilarity(Models[0].SubFiles[0].Name, new FileInfo(movieList[0]).Name) > 30)
+                        (movieList, originalMovieList) = (originalMovieList, movieList);
+
+        if (originalMovieList.Count > 0) AddOriginalMovie(originalMovieList);
+        if (movieList.Count > 0) AddMovie(movieList);
     }
 }
